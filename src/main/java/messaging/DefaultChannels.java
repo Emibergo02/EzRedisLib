7de48354d38def913ae3451ca41965b99ca79979
Emibergo02 <36164338+Emibergo02@users.noreply.github.com>
2022-06-08 16:05:48 +0200

    initial commit

--- src/main/java/messaging/DefaultChannels.java
+++ src/main/java/messaging/DefaultChannels.java
@@ -1,6 +1,6 @@
 /*
- * BedWars1058 - A bed wars mini-game.
- * Copyright (C) 2022 Andrei Dascălu
+ * EzRedisLib - A redis library.
+ * Copyright (C) 2022 Emiliano Bergonzani
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,7 +15,7 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
  *
- *  Contact e-mail: andrew.dascalu@gmail.com
+ *  Contact e-mail: emibergo@gmail.com
  */
 
 package messaging;
--- src/main/java/messaging/RedisMessagingHandler.java
+++ src/main/java/messaging/RedisMessagingHandler.java
@@ -1,3 +1,23 @@
+/*
+ * EzRedisLib - A redis library.
+ * Copyright (C) 2022 Emiliano Bergonzani
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ *  Contact e-mail: emibergo@gmail.com
+ */
+
 package messaging;
 
 import com.google.gson.Gson;
