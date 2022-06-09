c0fbc359fe1bd5e314272e3169d6bb813d30d4ee
Emibergo02 <36164338+Emibergo02@users.noreply.github.com>
2022-06-09 18:07:01 +0200

    jitpack integration

--- src/main/java/examples/Main.java
+++ src/main/java/examples/Main.java
@@ -2,13 +2,15 @@ package examples;
 
 import ezredislib.RedisMessagingHandler;
 import ezredislib.channel.DefaultChannels;
+import ezredislib.channel.PubSubListener;
+import ezredislib.packet.PingPacket;
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
-    public static void main(String[] args) throws InstantiationException {
+    public static void main(String[] args) throws InstantiationException, InterruptedException {
 
 
         //MAIN ACCESS POINTS
@@ -28,6 +30,17 @@ public class Main {
             handler.sendPacketAsync(DefaultChannels.CHANNEL_A.getName(), packet);
         }
         , 0, 100, TimeUnit.MILLISECONDS);
+
+        //Compact anonymous version
+        RedisMessagingHandler handler2 = new RedisMessagingHandler("localhost", 6379, null, null);
+        handler2.registerChannelListener(new PubSubListener<PingPacket>("channel1") {
+
+            @Override
+            public void read(PingPacket message) {
+                System.out.println("Packet timestamp: "+ message.getTimestamp()+" current:"+System.currentTimeMillis());
+            }
+        });
+        handler2.sendPacket("channel1", new PingPacket("client1", "client2"));
 
 
     }
